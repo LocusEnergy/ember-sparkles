c7a020aa6238c0230d9e01da6615e59cda66fda7
zigahertz <zach.zibrat@locusenergy.com>
2016-04-15 21:37:36 -0700

    x coordinate works in SVG rect

--- addon/helpers/ember-sparkles/d3-select.js
+++ addon/helpers/ember-sparkles/d3-select.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export function emberSparklesD3Select(element) {
-  console.log('element', element);
   return d3.select(element);
 }
 
--- tests/integration/components/ember-sparkles/bar-chart-test.js
+++ tests/integration/components/ember-sparkles/bar-chart-test.js
@@ -5,6 +5,8 @@ moduleForComponent('ember-sparkles/bar-chart', 'Integration | Component | ember 
   integration: true
 });
 
+let getAttr = (collection, attribute) => collection.map((idx, item) => $(item).attr(attribute)).toArray();
+
 test('it renders', function(assert) {
   this.set('data', []);
   this.render(hbs`<svg>{{ember-sparkles/bar-chart data=data}}</svg>`);
@@ -12,11 +14,13 @@ test('it renders', function(assert) {
 });
 
 test('it accepts data argument', function(assert) {
-  let data = [ [ '2010-10-11', 20 ], [ '2010-10-12', 15 ], [ '2010-10-13', 25] ];
+  // TODO: use MomentJS objects
+  // check if works with D3 date parse function, so can pass strings as dates? maybe this is in d3-scale library?
+  let data = [ [ new Date('2010-10-11'), 20 ], [ new Date('2010-10-12'), 15 ], [ new Date('2010-10-13'), 25] ];
 
   this.set('data', data);
 
-  this.set('xDomain', ['2010-10-11', '2010-10-13']);
+  this.set('xDomain', [new Date('2010-10-11'), new Date('2010-10-13')]);
   this.set('xRange', [0, 100]);
 
   this.set('yDomain', [15, 25]);
@@ -28,9 +32,15 @@ test('it accepts data argument', function(assert) {
     </svg>`
   );
 
-  stop()
-  assert.equal(this.$('rect').length, 3, 'there are 3 <rect> elements');
-  // there are 3 bars
-  // bar 1 has height 20 at date 2010/10/11, bar 2 has height 15 at date... , bar 3 has height 25 at date
+  let rect = this.$('rect');
+  let xCoordinates = getAttr(rect, 'x');
+  let yCoordinates = getAttr(rect, 'y');
 
+  assert.equal(rect.length, 3, 'there are 3 <rect> elements');
+  assert.deepEqual(xCoordinates, ['0', '50', '100'], 'each rectangle has an x coordinate, and they evenly split the range');
+  // assert.deepEqual(yCoordinates, ['0', '50', '100'], 'each rectangle has a y coordinate');
 });
+
+// test that data can be updated
+// test width of rectangles
+// test height of rectangles
