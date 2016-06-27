b497939bcce289de4b612aaeff97c655587d94b7
Albert Guo <albert.guo@locusenergy.com>
2016-06-27 16:42:56 -0700

    Added Pie-Chart Controller to generate Random Data

--- tests/dummy/app/controllers/pie-chart.js
+++ tests/dummy/app/controllers/pie-chart.js
@@ -1,19 +1,24 @@
-// import Ember from 'ember';
-// import _ from 'lodash/lodash';
-//
-// let generateData = function() {
-//   let
-// };
-//
-//
-// export default Ember.Controller.extend({
-//   init() {
-//     this.set('pieData', generateData());
-//   },
-//
-//   actions: {
-//     toggleData() {
-//       this.set('pieData', generateData());
-//     }
-//   }
-// });
+import Ember from 'ember';
+import _ from 'lodash/lodash';
+
+let generateData = function() {
+  let ages = ['arc 1', 'arc 2', 'arc 3', 'arc 4', 'arc 5', 'arc 6'];
+  return ages.map( age => {
+      return {
+        age,
+        population: _.random(1, 100)
+      };
+  });
+};
+
+
+export default Ember.Controller.extend({
+  init() {
+    this.set('pieData', generateData());
+  },
+  actions: {
+    toggleData() {
+      this.set('pieData', generateData());
+    }
+  }
+});
