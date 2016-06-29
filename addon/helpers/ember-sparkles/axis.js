ec942cd68c8f99bb824b08fb3b5112073a43acc3
Dig <dig.vargas@locusenergy.com>
2016-06-29 16:46:56 -0700

    attemtps to use helper to no avail

--- addon/helpers/ember-sparkles/axis.js
+++ addon/helpers/ember-sparkles/axis.js
@@ -1,28 +1,27 @@
 import Ember from 'ember';
 import axis from 'd3-axis';
 import { timeFormat } from 'd3-time-format';
-import { d3TickFormat } from 'ember-d3-helpers';
 
 const { String: { capitalize } } = Ember;
 
-export function emberSparklesAxis([ scale ], { position, tickPresentation: tickFormat, ticks, width, height }) {
+export function emberSparklesAxis([ scale ], { position, tickFormat, ticks, width, height }) {
   let axisType = `axis${capitalize(position)}`;
   let axisFn = axis[axisType];
 
   let result = axisFn().scale(scale);
 
-  // switch(typeof tickFormat) {
-  //   case 'string':
-  //     result.tickFormat(timeFormat(tickFormat));
-  //     break;
-  //   case 'function':
-  //     result.tickFormat(tickFormat(timeFormat));
-  //     break;
-  // }
+  switch(typeof tickFormat) {
+    case 'string':
+      result.tickFormat(timeFormat(tickFormat));
+      break;
+    case 'function':
+      result.tickFormat(tickFormat(timeFormat));
+      break;
+  }
 
-  // ember-d3-helpers/d3-tick-format
-
-  result.tickFormat(d3TickFormat(tickPresentation));
+  // console.log('axis.js tickFormat - are you undefined?: ', tickFormat);
+  //
+  // result.tickFormat(tickFormat);
 
   if (ticks) {
     result.ticks(ticks);
