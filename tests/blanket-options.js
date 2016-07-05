48f0640124e7db1fd0a5d6a7d8bf23261ab7dcf1
zigahertz <zach.zibrat@locusenergy.com>
2016-07-05 14:26:33 -0700

    adds ember-addon-genie

---
+++ tests/blanket-options.js
@@ -0,0 +1,19 @@
+/* globals blanket, module */
+
+var options = {
+  modulePrefix: '',
+  filter: '//.*ember-sparkles/.*/',
+  antifilter: '//.*(tests|template).*/',
+  loaderExclusions: [],
+  enableCoverage: true,
+  cliOptions: {
+    reporters: ['lcov'],
+    autostart: true
+  }
+};
+
+if (typeof exports === 'undefined') {
+  blanket.options(options);
+} else {
+  module.exports = options;
+}
