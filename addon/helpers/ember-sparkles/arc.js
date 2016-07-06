06f6cee75725eaade88a83b081794e3e54f694d1
Albert Guo <albert.guo@locusenergy.com>
2016-07-06 11:11:38 -0700

    Addresses changes to Arc Helper

--- addon/helpers/ember-sparkles/arc.js
+++ addon/helpers/ember-sparkles/arc.js
@@ -1,13 +1,8 @@
 import Ember from 'ember';
 import { arc } from 'd3-shape';
 
-export function emberSparklesArc([ radius ], textLabel=false) {
-  if (textLabel === true){
-    return arc().outerRadius(radius - 40).innerRadius(radius - 40);
-  }
-  else {
-    return arc().outerRadius(radius-10).innerRadius(0);
-  }
+export function emberSparklesArc([ innerRadius, outerRadius ]) {
+    return arc().outerRadius(outerRadius).innerRadius(innerRadius);
 }
 
 export default Ember.Helper.helper(emberSparklesArc);
