e30ca2489613bd6136020a9d5936c7a8c7a018c9
zigahertz <zach.zibrat@locusenergy.com>
2016-07-05 18:28:43 -0700

    removes unused code

--- config/release.js
+++ config/release.js
@@ -3,18 +3,6 @@
 
 // For details on each option run `ember help release`
 module.exports = {
-  init: function() {
-    this._previousVersion = require('../package.json').version;
-  },
-
-  afterPush: function(project, tags) {
-    runCommand('ember genie:changelog --write=true --version=' + this._previousVersion + ' --new-version=' + tags.next, true);
-  },
-
-  afterPublish: function(project, versions) {
-    runCommand('ember github-pages:commit --message "Released ' + versions.next + '"', true);
-    runCommand('git push origin gh-pages:gh-pages', true);
-  },
   // local: true,
   // remote: 'some_remote',
   // annotation: "Release %@",
