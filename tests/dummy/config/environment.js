9c0dc04686384fbe55f8d3272a727996c7831c8e
zigahertz <zach.zibrat@locusenergy.com>
2016-07-05 17:01:10 -0700

    wip

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -39,7 +39,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // ENV.locationType = 'hash';
+    ENV.locationType = 'hash';
     ENV.rootURL = '/ember-sparkles/';
   }
 
