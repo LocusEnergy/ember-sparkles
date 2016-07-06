c73a6002c6c4319114496fb23facb14ea37f3a0b
zigahertz <zach.zibrat@locusenergy.com>
2016-07-05 17:32:52 -0700

    wip

--- config/deploy.js
+++ config/deploy.js
@@ -1,8 +1,8 @@
-module.exports = function(deployTarget) {  
-  return {
-    pagefront: {
-      app: 'ember-sparkles',
-      key: process.env.PAGEFRONT_KEY
-    }
-  };
+module.exports = function(deployTarget) {
+  // return {
+  //   pagefront: {
+  //     app: 'ember-sparkles',
+  //     key: process.env.PAGEFRONT_KEY
+  //   }
+  // };
 };
