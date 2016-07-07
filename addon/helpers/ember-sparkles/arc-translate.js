e156e0825f207c21c68cb2d16ac3dbf47a4d1827
Albert Guo <albert.guo@locusenergy.com>
2016-07-07 16:14:09 -0700

    Added Text to Pie Chart

--- addon/helpers/ember-sparkles/arc-translate.js
+++ addon/helpers/ember-sparkles/arc-translate.js
@@ -1,8 +1,16 @@
 import Ember from 'ember';
 
-export function emberSparklesArcTranslate([ centroid ]) {
+export function emberSparklesArcTranslate([ centroid ], { height, width }) {
   return function(d) {
-    return `translate(${centroid(d)})`;
+    let x, y;
+    [x, y] = centroid(d)
+    x = x + width;
+    y = y + height;
+    let coordinates = [];
+    coordinates.push(x);
+    coordinates.push(y);
+    console.log(coordinates)
+    return `translate(${coordinates})`;
   };
 }
 
--- tests/unit/helpers/ember-sparkles/arc-translate-test.js
+++ tests/unit/helpers/ember-sparkles/arc-translate-test.js
@@ -1,10 +1,12 @@
 import { emberSparklesArcTranslate } from 'dummy/helpers/ember-sparkles/arc-translate';
 import { module, test } from 'qunit';
+import { arc  } from 'd3-shape'
 
 module('Unit | Helper | ember sparkles/arc translate');
-
+let testArc = arc().innerRadius(10).outerRadius(10);
 // Replace this with your real tests.
 test('it works', function(assert) {
-  let result = emberSparklesArcTranslate([42]);
-  assert.ok(result);
+  let result = emberSparklesArcTranslate([testArc.centroid]);
+  console.log(result)
+  assert.ok(testArc);
 });
