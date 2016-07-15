b88ac6b1342c0e62c1826783401b73f995f6b659
Albert Guo <albert.guo@locusenergy.com>
2016-07-15 12:45:47 -0700

    removed comments

--- tests/unit/helpers/pie-sparkler-test.js
+++ tests/unit/helpers/pie-sparkler-test.js
@@ -4,12 +4,6 @@ import _ from 'lodash';
 
 module('Unit | Helper | pie sparkler');
 
-// Replace this with your real tests.
-//Tests to Write
-//1. Test that it displays data when given percentages all over the threshold//done
-//2. Test that the precision works the way it's supposed to//done
-//3. Test that the percetages add up to roughly 100//done
-//4. Test that the there are missing percentages under a certain threshold
 test('pie sparkler works as expected', function(assert) {
   let data = [
     {
@@ -55,17 +49,4 @@ test('pie sparkler works as expected', function(assert) {
   result = pieSparkler([ data ], { dataKey: 'value', threshold: 10 });
   percentages = result.map(({ percentage }) => percentage);
   assert.deepEqual(percentages, ["39.2", "30.1", "30.1", undefined], 'The percentages are left out if they do not exist');
-  // assert.equal(result, [{
-  //   key: 'arc 1',
-  //   percentage: '39.4%',
-  //   value: 65,
-  // },{
-  //   key: "arc 2",
-  //   percentage: "30.3%",
-  //   value: 50
-  // },{
-  // key: "arc 3",
-  // percentage: "30.3%",
-  // value: 50
-  // }]);
 });
