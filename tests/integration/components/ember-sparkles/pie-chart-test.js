f7af2049994c31be6de21962d6ac7a6ae979af11
Albert Guo <albert.guo@locusenergy.com>
2016-07-15 10:52:51 -0700

    Pie Test Working, working on Mapping Domain

--- tests/integration/components/ember-sparkles/pie-chart-test.js
+++ tests/integration/components/ember-sparkles/pie-chart-test.js
@@ -37,7 +37,6 @@ test('The chart accepts data and generates arcs properly', function(assert) {
     }
   ]);
 
-
   this.set('domain', ['arc 1', 'arc 2', 'arc 3']);
 
   this.render(hbs`
@@ -57,48 +56,57 @@ test('The chart accepts data and generates arcs properly', function(assert) {
   assert.equal(this.$('path').length, 3, 'There are the correct number of arcs');
 });
 
-// test('data can be updated and removed', function(assert) {
-//   assert.expect(2);
-//   this.set('data', [
-//     {
-//       key: 'arc 1',
-//       value: 50
-//     },{
-//       key: 'arc 2',
-//       value: 50
-//     }, {
-//       key: 'arc 3',
-//       value: 50
-//     }
-//   ]);
-//
-//   this.render(hbs`
-//   <svg>
-//     {{ember-sparkles/pie-chart
-//         data=data
-//         with-transition=false
-//         domain=(map-by 'value' data)
-//       }}
-//   </svg>
-//   `);
-//   assert.equal(this.$('path').length, 3, 'There are intially 3 arcs');
-//   //Information updated
-//   this.set('data', [
-//     {
-//       key: 'arc 1',
-//       value: 50
-//     },{
-//       key: 'arc 2',
-//       value: 50
-//     }, {
-//       key: 'arc 3',
-//       value: 50
-//     }, {
-//       key: 'arc 4',
-//       value: 50
-//     }
-//   ]);
-//   assert.equal(this.$('path').length, 4, 'There are the correct number of arcs after the data is updated');
-//
-//
-// });
+test('data can be updated and removed', function(assert) {
+  assert.expect(2);
+  this.set('data', [
+    {
+      key: 'arc 1',
+      value: 50
+    },{
+      key: 'arc 2',
+      value: 50
+    }, {
+      key: 'arc 3',
+      value: 50
+    }
+  ]);
+
+  this.set('domain', ['arc 1', 'arc 2', 'arc 3']);
+
+  this.render(hbs`
+  <svg height="100" width="100">
+    {{ember-sparkles/pie-chart
+      height=50
+      width=50
+      radius=10
+      colorScale=(cat-color-scale '20' domain)
+      domain=domain
+      data=(pie-sparkler data dataKey='value' threshold=5 precision=1)
+      with-transition=false
+      with-arc-labels=true
+      }}
+  </svg>
+  `);
+  assert.equal(this.$('path').length, 3, 'There are intially 3 arcs');
+  //Information updated
+  this.set('data', [
+    {
+      key: 'arc 1',
+      value: 50
+    },{
+      key: 'arc 2',
+      value: 50
+    }, {
+      key: 'arc 3',
+      value: 50
+    }, {
+      key: 'arc 4',
+      value: 50
+    }
+  ]);
+
+  this.set('domain', ['arc 1', 'arc 2', 'arc 3', 'arc 4']);
+  assert.equal(this.$('path').length, 4, 'There are the correct number of arcs after the data is updated');
+
+
+});
