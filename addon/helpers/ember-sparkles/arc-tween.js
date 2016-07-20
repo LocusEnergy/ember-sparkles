c0133d3ddf76235c506bf85940ee3cae9aba15ae
Albert Guo <albert.guo@locusenergy.com>
2016-07-20 11:31:10 -0700

    ArcTween now does not output errors

--- addon/helpers/ember-sparkles/arc-tween.js
+++ addon/helpers/ember-sparkles/arc-tween.js
@@ -2,14 +2,12 @@ import Ember from 'ember';
 import { arc } from 'd3-shape';
 import { interpolate } from 'd3-interpolate';
 
-export function emberSparklesArcTween([ newAngle ]) {
-  return function(d) {
-    var interpolate = interpolate(d.endAngle , newAngle );
-    return function(t) {
-      d.endAngle = interpolate(t)
-      return arc(d);
-    };
-  }
+export function emberSparklesArcTween(params) {
+  params.innerRadius = 0;
+  var i = interpolate({startAngle: 0, endAngle: 0}, params);
+  return function(t) {
+    return arc(i(t));
+  };
 }
 
 export default Ember.Helper.helper(emberSparklesArcTween);
