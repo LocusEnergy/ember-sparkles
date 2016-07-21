66c41ec3df5fdff567eb628a3cb03f713592ed14
Albert Guo <albert.guo@locusenergy.com>
2016-07-21 14:50:24 -0700

    No more failing tests

--- tests/unit/helpers/ember-sparkles/arc-translate-test.js
+++ tests/unit/helpers/ember-sparkles/arc-translate-test.js
@@ -4,7 +4,8 @@ import { arc  } from 'd3-shape'
 
 module('Unit | Helper | ember sparkles/arc translate');
 let testArc = arc().innerRadius(10).outerRadius(10);
+let result = emberSparklesArcTranslate([ testArc.centroid ], 10, 10)
 // Replace this with your real tests.
 test('it works', function(assert) {
-  assert.ok(testArc)
+  assert.equal(typeof result, 'function', 'The helper returns a function');
 });
