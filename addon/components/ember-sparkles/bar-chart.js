6b8b81b9e26c7ba9c08ce0e5d5cf70a0ef55e313
Taras Mankovski <tarasm@gmail.com>
2016-07-30 05:53:55 +0530

    Updated bar chart to use d3-graph

--- addon/components/ember-sparkles/bar-chart.js
+++ addon/components/ember-sparkles/bar-chart.js
@@ -1,21 +1,9 @@
 import Ember from 'ember';
-import { select } from 'd3-selection';
 import layout from '../../templates/components/ember-sparkles/bar-chart';
 
 export default Ember.Component.extend({
   layout,
-  tagName: 'g',
-  classNames: ['ember-sparkles--bar-chart'],
+  tagName: '',
 
-  'with-transition': true,
-
-  didInsertElement() {
-    this._super(...arguments);
-    Ember.run.scheduleOnce('afterRender', this, 'renderChart');
-  },
-
-  renderChart() {
-    let [ el ] = this.$().toArray();
-    this.set('d3el', select(el));
-  }
+  'with-transition': true
 });
