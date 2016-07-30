d77558f27006639cd70d4ac972af9784473164ba
Taras Mankovski <tarasm@gmail.com>
2016-07-30 05:54:44 +0530

    Upgraded grouped-bar-chart to use d3-chart

--- addon/components/ember-sparkles/grouped-bar-chart.js
+++ addon/components/ember-sparkles/grouped-bar-chart.js
@@ -1,24 +1,12 @@
 import Ember from 'ember';
-import { select } from 'd3-selection';
 import layout from '../../templates/components/ember-sparkles/grouped-bar-chart';
 
 export default Ember.Component.extend({
   layout,
-  tagName: 'g',
-  classNames: ['ember-sparkles--grouped-bar-chart'],
+  tagName: '',
 
   'with-transition': true,
   'with-legend': true,
-
-  didInsertElement() {
-    this._super(...arguments);
-    Ember.run.scheduleOnce('afterRender', this, 'renderChart');
-  },
-
-  renderChart() {
-    let [ el ] = this.$().toArray();
-    this.set('d3el', select(el));
-  },
 
   // callback to position legend
   transformLegend(d, i) {
