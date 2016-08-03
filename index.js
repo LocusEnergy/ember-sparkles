9f412ef666afa6b988c9e154cbbb27f77f9ebb57
Taras Mankovski <tarasm@gmail.com>
2016-08-03 23:26:49 +0530

    Use `{{d3-graph}}` component to render graphs (#26)
    * Updated bar chart to use d3-graph
    
    * Upgraded grouped-bar-chart to use d3-chart
    
    * Updated link-chart to use d3-chart
    
    * Updated pie-chart to use d3-graph component
    
    * upgrades to latest ember-d3-helpers
    
    * Adding app plugin
    
    * Calling super after app
    
    * try a fix
    
    * Back to release

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-sparkles'
+  name: 'ember-sparkles',
+  included: function(app) {
+    if (app.app) {
+      app = app.app;
+    }
+    this.app = app;
+
+    this._super.included.apply(this, arguments);
+  }
 };
