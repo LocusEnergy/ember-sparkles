85845eec4520b2f739ff77af10520495597aea6b
zigahertz <zach.zibrat@locusenergy.com>
2016-08-15 17:56:13 -0700

    contextual legend done

--- addon/helpers/e-s/apply-method.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export function eSApplyMethod([ partialFn, ...args ], { method }) {
-  partialFn[method].call(null, ...args);
-  return partialFn;
-}
-
-export default Ember.Helper.helper(eSApplyMethod);
