afcc7c3de0496982fa7e6b0f4c3ced4920db184d
zigahertz <zach.zibrat@locusenergy.com>
2016-09-17 10:24:05 -0700

    done

--- addon/components/e-s/bar-chart.js
+++ addon/components/e-s/bar-chart.js
@@ -3,7 +3,6 @@ import layout from '../../templates/components/e-s/bar-chart';
 
 export default Ember.Component.extend({
   layout,
-  classNames: ['ember-sparkles--bar-chart'],
   tagName: '',
   height: 100,
   width: 100,
