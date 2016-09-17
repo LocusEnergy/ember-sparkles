e91de3704ffa9649ae9904a26de3165065d2da77
zigahertz <zach.zibrat@locusenergy.com>
2016-09-17 01:21:49 -0700

    begins

--- addon/components/e-s/bar-chart.js
+++ addon/components/e-s/bar-chart.js
@@ -3,6 +3,7 @@ import layout from '../../templates/components/e-s/bar-chart';
 
 export default Ember.Component.extend({
   layout,
+  classNames: ['ember-sparkles--bar-chart'],
   tagName: '',
   height: 100,
   width: 100,
--- addon/components/e-s/grouped-bar-chart.js
+++ addon/components/e-s/grouped-bar-chart.js
@@ -3,6 +3,7 @@ import layout from '../../templates/components/e-s/grouped-bar-chart';
 
 export default Ember.Component.extend({
   layout,
+  classNames: ['ember-sparkles--grouped-bar-chart'],
   tagName: '',
   height: 100,
   width: 100,
