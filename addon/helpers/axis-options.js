633e29c8cc8e0f85bf2529868cde2b8c19826049
zigahertz <zach.zibrat@locusenergy.com>
2016-11-14 16:00:07 -0800

    improves syntax

--- addon/helpers/axis-options.js
+++ addon/helpers/axis-options.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
 export function axisOptions([ position, width, height, x=0, y=0 ]) {
-  let w = position != 'right' ? 0 : width;
-  let h = position != 'bottom' ? 0 : height;
+  let w = position === 'right' ? width : 0;
+  let h = position === 'bottom' ? height : 0;
   let gridLength = position === ('left' || 'right') ? width : height;
   let translate = `translate(${w + x},${h + y})`;
   return { gridLength, translate };
