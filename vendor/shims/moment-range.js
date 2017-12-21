14c24a7febdd5d0a360805704d8a234a6a65a2e9
Iaroslav Popov <iaroslav.popov@zalando.de>
2017-12-21 12:40:43 +0100

    updated dependencies and fixed code

---
+++ vendor/shims/moment-range.js
@@ -0,0 +1,12 @@
+(function() {
+  function vendorModule() {
+    'use strict';
+
+    return {
+      'default': self['moment-range'],
+      __esModule: true,
+    };
+  }
+
+  define('moment-range', [], vendorModule);
+})();
